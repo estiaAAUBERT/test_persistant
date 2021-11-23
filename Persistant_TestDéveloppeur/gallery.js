6b91641109e3754b8d41004960c5b5df58ae61a1
Augustin AUBERT <a.aubert@estia.local>
2021-11-23 16:13:36 +0100

    test

---
+++ Persistant_TestDéveloppeur/gallery.js
@@ -0,0 +1,14 @@
+/* global bootstrap: false */
+(function () {
+  'use strict'
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
+
+function remove_image(){
+  document.getElementById('img').remove();
+}
+
+
